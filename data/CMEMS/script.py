e92cf6a8229325bc1c8c860a1ad3042ca84bf3ab
Walk <2757412961@qq.com>
2022-12-13 16:44:55 +0800

    [add] CMEMS

---
+++ data/CMEMS/script.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+@File  : script.py
+@Author: Zjh
+@Date  : 2022/12/13
+@Update: 2022/12/13
+@Desc  :
+"""
+
+"""
+motuclient --motu https://nrt.cmems-du.eu/motu-web/Motu --service-id GLOBAL_ANALYSIS_FORECAST_BIO_001_028-TDS --product-id global-analysis-forecast-bio-001-028-daily --date-min "2021-01-01 00:00:00" --date-max "2021-02-01 23:59:59" --depth-min 0.49402499198913574 --depth-max 0.49402499198913574 --variable chl --variable dissic --variable ph --variable talk --variable spco2 --out-dir "F:\Ocean\CMEMS\Global Ocean Biogeochemistry Analysis and Forecast" --out-name MULTIOBS_GLO_BIO_CARBON_SURFACE_REP_015_008-2021-01.nc --user aturing1 --pwd MoYan0000
+"""
