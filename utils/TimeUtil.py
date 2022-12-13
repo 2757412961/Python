d6f777e0cb41cd5d3907110b2363e4a4fd33194d
Walk <2757412961@qq.com>
2022-12-13 21:26:08 +0800

    [add] Time by month

--- utils/TimeUtil.py
+++ utils/TimeUtil.py
@@ -6,6 +6,7 @@
 # @Desc  :
 import time
 import datetime
+import pandas as pd
 
 
 def get_time():
@@ -17,7 +18,7 @@ def get_format_time():
     return currentTime
 
 
-def traversal_date():
+def traversal_days():
     begin = datetime.date(2003, 1, 1)
     end = datetime.date(2022, 1, 1)
 
@@ -27,3 +28,20 @@ def traversal_date():
         month = time.strftime("%m")  # 01
         day = time.strftime("%d")  # 01
         print(time)
+
+
+def traversal_months():
+    begin = datetime.date(2003, 8, 1)
+    end = datetime.date(2004, 4, 1)
+
+    periods = (end.year - begin.year) * 12 + (end.month - begin.month)
+    time_series = pd.period_range(begin, periods=periods, freq='M')
+
+    for t in time_series:  # 2003-08
+        start_time = t.start_time  # 2003-08-01
+        end_time = t.end_time  # 2003-08-31
+        ############################################
+        year = start_time.strftime("%Y")  # 2003
+        month = start_time.strftime("%m")  # 01
+        day = start_time.strftime("%d")  # 01
+        print(t, year, month, day)
